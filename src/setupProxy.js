4ffec4c048ae8105842b9db07068aa05d8b5c62c
Farshad <farshad.basereh@gmail.com>
2020-09-14 21:46:30 -0400

    v1.4

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-module.exports = function (app) {
-  app.use(
-    ["/contact"],
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-    })
-  );
-};
